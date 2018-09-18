51f648dfda827f310f688133c81e10d5c822067c
Sherry Martin <sm674@cornell.edu>
2018-09-18 17:17:31 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     value = programmer_hash[:dennis_ritchie][:languages].first
+     value = programmer_hash[:dennis_ritchie][:languages].first #.first 
 end
 
 def adding_matz
